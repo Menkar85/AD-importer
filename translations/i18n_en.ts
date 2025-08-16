22d28178af310858844ec94013874efc902c877e
Menkar <menkar85@gmail.com>
2025-08-16 19:05:20 +1000

    add source files to github

---
+++ translations/i18n_en.ts
@@ -0,0 +1,244 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.py" line="131"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="132"/>
+        <source>Software for batch import of users into Active Directory.</source>
+        <translation>Software for batch import of users into Active Directory.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="151"/>
+        <location filename="../mainwindow.py" line="236"/>
+        <source>Success</source>
+        <translation>Success</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="152"/>
+        <source>Server name successfully validated.</source>
+        <translation>Server name successfully validated.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="157"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="158"/>
+        <source>Server not found! Please check server name or that PC is in domain.</source>
+        <translation>Server not found! Please check server name or that PC is in domain.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="169"/>
+        <source>Excel files (*.xlsx *.xls)</source>
+        <translation>Excel files (*.xlsx *.xls)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="182"/>
+        <source>Excel files (*.xlsx)</source>
+        <translation>Excel files (*.xlsx)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="201"/>
+        <location filename="../mainwindow.py" line="244"/>
+        <source>Warning</source>
+        <translation>Warning</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="202"/>
+        <source>Next step will apply changes to AD.
+Please verify correct destination OU and confirm user import.</source>
+        <translation>Next step will apply changes to AD.
+Please verify correct destination OU and confirm user import.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="237"/>
+        <source>Import completed.</source>
+        <translation>Import completed.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="266"/>
+        <source>Confirmation</source>
+        <translation>Confirmation</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="267"/>
+        <source>Do you really want to quit?</source>
+        <translation>Do you really want to quit?</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidget</name>
+    <message>
+        <location filename="../settings.py" line="29"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="19"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="36"/>
+        <source>Log folder:</source>
+        <translation>Log folder:</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="73"/>
+        <source>Browse</source>
+        <translation>Browse</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="97"/>
+        <source>Keep Active Directory configuration on application close</source>
+        <translation>Keep Active Directory configuration on application close</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="120"/>
+        <source>Protocol:</source>
+        <translation>Protocol:</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="172"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="179"/>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="186"/>
+        <source>Apply</source>
+        <translation>Apply</translation>
+    </message>
+</context>
+<context>
+    <name>main_window</name>
+    <message>
+        <location filename="../mainwindow.ui" line="19"/>
+        <source>MainWindow</source>
+        <translation>MainWindow</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="35"/>
+        <source>Active Directory Configuration</source>
+        <translation>Active Directory Configuration</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="50"/>
+        <source>AD Server name:</source>
+        <translation>AD Server name:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="62"/>
+        <source>User name:</source>
+        <translation>User name:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>Password:</source>
+        <translation>Password:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="86"/>
+        <source>UPN-suffix:</source>
+        <translation>UPN-suffix:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="105"/>
+        <source>Test</source>
+        <translation>Test</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="143"/>
+        <source>Import configuration</source>
+        <translation>Import configuration</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="158"/>
+        <source>Source file:</source>
+        <translation>Source file:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="170"/>
+        <source>Destination OU:</source>
+        <translation>Destination OU:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="182"/>
+        <source>Save result to:</source>
+        <translation>Save result to:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="202"/>
+        <source>Browse</source>
+        <translation>Browse</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="223"/>
+        <source>Save as</source>
+        <translation>Save as</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="266"/>
+        <source>Preview file</source>
+        <translation>Preview file</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="288"/>
+        <source>Start importing</source>
+        <translation>Start importing</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="310"/>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="339"/>
+        <location filename="../mainwindow.ui" line="408"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="350"/>
+        <source>Language</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="357"/>
+        <location filename="../mainwindow.ui" line="394"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="376"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="385"/>
+        <source>Russian</source>
+        <translation>Russian</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="403"/>
+        <source>About Qt</source>
+        <translation>About Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="413"/>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/i18n_ru.ts
@@ -0,0 +1,246 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.py" line="133"/>
+        <source>About</source>
+        <translation>О приложении</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="134"/>
+        <source>Software for batch import of users into Active Directory.</source>
+        <translation>Приложение для пакетного создания пользователей в Активной Директории из шаблона.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="153"/>
+        <location filename="../mainwindow.py" line="239"/>
+        <source>Success</source>
+        <translation>Успешно</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="154"/>
+        <source>Server name successfully validated.</source>
+        <translation>Сервер найден.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="159"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="160"/>
+        <source>Server not found! Please check server name or that PC is in domain.</source>
+        <translation>Сервер не найден! Пожалуйта проверьте правильность имени контроллера домена,
+убедитель, что компьютер подключен к домену и вход осуществлен пользователем домена.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="171"/>
+        <source>Excel files (*.xlsx *.xls)</source>
+        <translation>Файлы excel (*.xlsx *.xls)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="184"/>
+        <source>Excel files (*.xlsx)</source>
+        <translation>Файлы excel (*.xlsx)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="203"/>
+        <location filename="../mainwindow.py" line="247"/>
+        <source>Warning</source>
+        <translation>Внимание</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="205"/>
+        <source>Next step will apply changes to AD.
+Please verify correct destination OU and confirm user import.</source>
+        <translation>Следующий шаг применит изменения к Директории.
+Пожалуйста, убедитесь в правильности контейнера назначения и 
+нажмите OK.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="240"/>
+        <source>Import completed.</source>
+        <translation>Импорт завершен.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="269"/>
+        <source>Confirmation</source>
+        <translation>Подтверждение</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="270"/>
+        <source>Do you really want to quit?</source>
+        <translation>Вы действительно хотите выйти?</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidget</name>
+    <message>
+        <location filename="../settings.py" line="29"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="19"/>
+        <source>Form</source>
+        <translation>Форма</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="36"/>
+        <source>Log folder:</source>
+        <translation>Папка логов:</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="73"/>
+        <source>Browse</source>
+        <translation>Обзор</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="97"/>
+        <source>Keep Active Directory configuration on application close</source>
+        <translation>Сохранять настройки Директории при закрытии</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="120"/>
+        <source>Protocol:</source>
+        <translation>Протокол:</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="172"/>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="179"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="186"/>
+        <source>Apply</source>
+        <translation>Применить</translation>
+    </message>
+</context>
+<context>
+    <name>main_window</name>
+    <message>
+        <location filename="../mainwindow.ui" line="19"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="35"/>
+        <source>Active Directory Configuration</source>
+        <translation>Конфигурация Active Directory</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="50"/>
+        <source>AD Server name:</source>
+        <translation>Контроллер домена:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="62"/>
+        <source>User name:</source>
+        <translation>Имя пользователя:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="86"/>
+        <source>UPN-suffix:</source>
+        <translation>UPN-суффикс:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="105"/>
+        <source>Test</source>
+        <translation>Тест</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="143"/>
+        <source>Import configuration</source>
+        <translation>Настройки импорта</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="158"/>
+        <source>Source file:</source>
+        <translation>Файл-источник:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="170"/>
+        <source>Destination OU:</source>
+        <translation>OU Назначения:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="182"/>
+        <source>Save result to:</source>
+        <translation>Файл результата:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="202"/>
+        <source>Browse</source>
+        <translation>Выбрать</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="223"/>
+        <source>Save as</source>
+        <translation>Сохранить как</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="266"/>
+        <source>Preview file</source>
+        <translation>Просмотр</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="288"/>
+        <source>Start importing</source>
+        <translation>Начать импорт</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="310"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="339"/>
+        <location filename="../mainwindow.ui" line="408"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="350"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="357"/>
+        <location filename="../mainwindow.ui" line="394"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="376"/>
+        <source>English</source>
+        <translation>Английский</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="385"/>
+        <source>Russian</source>
+        <translation>Русский</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="403"/>
+        <source>About Qt</source>
+        <translation>О Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="413"/>
+        <source>Quit</source>
+        <translation>Выход</translation>
+    </message>
+</context>
+</TS>
